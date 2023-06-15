130ea5fb5b37cf3f9b9eccc302084fe457924b57
deny <denisson.graca3@gmail.com>
2023-06-15 00:39:24 -0300

    animations

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
+import * as Animatable from "react-native-animatable"
 import { useNavigation } from '@react-navigation/native'
 import { HeroImage } from '../assets'
 
@@ -17,16 +18,16 @@ const HomeScreen = () => {
     return (
         <SafeAreaView className={`bg-white flex-1 relative`}>
             {/* first */}
-            <View className={`flex-row px-6  mt-12 items-center space-x-2`}>
+            <View className={`flex-row px-6  mt-10 items-center space-x-2`}>
                 <View className={`bg-zinc-800 rounded-full w-16 h-16 items-center justify-center`}>
-                    <Text className={`text-white text-[#4DABB7] text-3xl font-semibold`}>Go</Text>
+                    <Text className={`text-[#4DABB7] text-3xl font-semibold`}>Go</Text>
                 </View>
 
                 <Text className={`text-[#2A2B4B] text-3xl font-semibold`}>Travel</Text>
             </View>
 
             {/* second */}
-            <View className={`px-6 mt-6 space-y-3`}>
+            <View className={`px-6 mt-4 space-y-3`}>
                 <Text className={`text-[#3C6072] text-[38px]`}>Enjoy The Trip with</Text>
                 <Text className={`text-[#00BCC9] text-[34px] font-bold`}>Good Moments</Text>
 
@@ -49,7 +50,9 @@ const HomeScreen = () => {
 
             {/* image */}
             <View className={`flex-1 relative items-center justify-center`}>
-                <Image
+                <Animatable.Image
+                    animation="fadeIn"
+                    easing="ease-in-out"
                     source={HeroImage}
                     className={`w-full h-full object-cover mt-16`}
 
@@ -58,13 +61,17 @@ const HomeScreen = () => {
                 {/* button */}
                 <View className={`
                     absolute bottom-20 w-24 h-24  rounded-full items-center justify-center
-                    border border-[#00BCC9] border-[2px]
+                    border-[#00BCC9] border-[2px]
                 `}>
-                    <View className={`
-                        w-20 h-20 items-center justify-center bg-[#00BCC9] rounded-full
-                    `}>
-                        <Text className={`text-white`}>Go</Text>
-                    </View>
+                    <TouchableOpacity>
+                        <Animatable.View 
+                            animation="pulse" easing="ease-in-out" iterationCount={"infinite"}
+                            className={`
+                                w-20 h-20 items-center justify-center bg-[#00BCC9] rounded-full
+                            `}>
+                                <Text className={`text-white text-[40px] font-semibold`}>Go</Text>
+                        </Animatable.View>
+                    </TouchableOpacity>
                 </View>
             </View>
 
